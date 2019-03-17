8247ee9a9405d4b463331ec1525c8c5c82a3a210
Shivansh <malasishivansh@gmail.com>
2019-03-17 21:10:45 +0530

    initial commit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class= "jumbotron text-center">
+     <h1>Welcome To Build!t</h1>
+     <p>let's start!!</p>
+    <p><a class="btn btn-primary btn-lg" href="/login">Login</a> <a class="btn btn-success btn-lg" href="/register">Register</a></p>
+</div>
+@endsection    
